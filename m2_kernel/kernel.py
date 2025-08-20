665ff06c595c20545e62df613f6a0776053cb8b0
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-08-19 22:29:16 -0700

    m2_kernel/kernel.py: Fix dataflow

--- m2_kernel/kernel.py
+++ m2_kernel/kernel.py
@@ -155,6 +155,7 @@ class M2Interp:
                 break
 
         while not EOT:
+            line = ''
             try:
                 for testline in self.proc:
                     line = testline[:-2]
