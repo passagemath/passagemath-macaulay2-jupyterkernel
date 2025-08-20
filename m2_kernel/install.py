822ed11a425543b9013d95aee4188eb605087ded
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-08-19 22:17:45 -0700

    m2_kernel/install.py: Remove use of notebook.nbextensions

--- m2_kernel/install.py
+++ m2_kernel/install.py
@@ -6,8 +6,6 @@ import sys
 
 from jupyter_client.kernelspec import KernelSpecManager
 from IPython.utils.tempdir import TemporaryDirectory
-from notebook import __path__ as notebook_dir
-from notebook.nbextensions import install_nbextension
 
 """ Macaulay2 Jupyter Kernel: standard jupyter kernel spec installation
 """
@@ -32,11 +30,6 @@ def install_kernel_assets(user=True, prefix=None):
         shutil.copy('{}/m2-spec/kernel.js'.format(assets_dir), td)
         print('Installing kernel spec ...')
         KernelSpecManager().install_kernel_spec(td, kernel_name='m2', user=user, prefix=prefix)
-
-    print("Installing nbextension for syntax highlighting ...")
-    install_nbextension('{}/m2-mode'.format(assets_dir),
-            nbextensions_dir='{}/static/components/codemirror/mode/'.format(notebook_dir[0]),
-            destination='macaulay2', overwrite=True, symlink=False)
 
 
 def _is_root():
